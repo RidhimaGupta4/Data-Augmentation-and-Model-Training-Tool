55df6d38889f4b4be81fadb89fe7f16146ed3fec
ridhimagu <ridhima1623@gmail.com>
2024-06-11 23:12:49 +0530

    resolved errors

---
+++ frontend/src/views/User.js
@@ -0,0 +1,167 @@
+import Upload from "components/Upload";
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col,
+} from "reactstrap";
+
+class User extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col md="6">
+              <Card className="card-user">
+                <div className="image">
+                  <img
+                    alt="..."
+                    src={require("assets/img/damir-bosnjak.jpg")}
+                  />
+                </div>
+                <CardBody>
+                  {/* <div className="author">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        className="avatar border-gray"
+                        src={require("assets/img/mike.jpg")}
+                      />
+                      <h5 className="title">Chet Faker</h5>
+                    </a>
+                    <p className="description">@chetfaker</p>
+                  </div> */}
+                  <p className="description text-center">
+                    <Upload></Upload>
+                  </p>
+                </CardBody>
+                {/* <CardFooter>
+                  <hr />
+                  <div className="button-container">
+                    <Row>
+                      <Col className="ml-auto" lg="3" md="6" xs="6">
+                        <h5>
+                          12 <br />
+                          <small>Files</small>
+                        </h5>
+                      </Col>
+                      <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                        <h5>
+                          2GB <br />
+                          <small>Used</small>
+                        </h5>
+                      </Col>
+                      <Col className="mr-auto" lg="3">
+                        <h5>
+                          24,6$ <br />
+                          <small>Spent</small>
+                        </h5>
+                      </Col>
+                    </Row>
+                  </div>
+                </CardFooter> */}
+              </Card>
+              {}
+            </Col>
+            <Col md="6">
+              <Card className="card-user">
+                <CardHeader>
+                  <CardTitle tag="h5">Add Augmentation</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Form>
+                    <Row>
+                      <Col className="pr-1" md="5">
+                        <FormGroup>
+                          <label>Brightness</label>
+                          <Input
+                            
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="px-1" md="3">
+                        <FormGroup>
+                          <label>Contrast</label>
+                          <Input
+                            
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="4">
+                        <FormGroup>
+                          <label htmlFor="exampleInputEmail1">Sharpness</label>
+                          <Input
+                            //placeholder="Email"
+                            type="email"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-1" md="6">
+                        <FormGroup>
+                          <label>Skewness</label>
+                          <Input
+                            
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-1" md="6">
+                        <FormGroup>
+                          <label>Add Noise to the image</label>
+                          <Input
+                            
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md="12">
+                        <FormGroup>
+                          
+                          <label>Weather Filters</label>
+                          <Input
+                            
+                            type="text"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <div className="update ml-auto mr-auto">
+                        <Button
+                          className="btn-round"
+                          color="primary"
+                          type="submit"
+                        >
+                          Add Augmentation
+                        </Button>
+                      </div>
+                    </Row>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
+}
+
+export default User;
