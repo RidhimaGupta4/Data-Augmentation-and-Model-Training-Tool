fa51f5d00dd1f099748f06eceeacb61948bf6a59
ridhimagu <ridhima1623@gmail.com>
2024-06-11 23:04:20 +0530

    update ui

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Nav } from "reactstrap";
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
+
+import logo from "icon.png";
+
+var ps;
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.activeRoute.bind(this);
+    this.sidebar = React.createRef();
+  }
+  // verifies if routeName is the one active (in browser input)
+  activeRoute(routeName) {
+    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  }
+  componentDidMount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps = new PerfectScrollbar(this.sidebar.current, {
+        suppressScrollX: true,
+        suppressScrollY: false,
+      });
+    }
+  }
+  componentWillUnmount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps.destroy();
+    }
+  }
+  render() {
+    return (
+      <div
+        className="sidebar"
+        data-color={this.props.bgColor}
+        data-active-color={this.props.activeColor}
+      >
+        <div className="logo">
+          <div className="logo-img">
+            <img
+              src={logo}
+              // src="https://cdn4.iconfinder.com/data/icons/miscellaneous-2-4/128/traffic_jam_road_highway-512.png"
+              alt="german-sign"
+              style={{ height: "150px", width: "150px",position:'relative',left:'20%' }}
+            />
+          </div>
+        </div>
+        <div className="sidebar-wrapper" ref={this.sidebar}>
+          <Nav>
+            {this.props.routes.map((prop, key) => {
+              return (
+                <li
+                  className={
+                    this.activeRoute(prop.path) +
+                    (prop.pro ? " active-pro" : "")
+                  }
+                  key={key}
+                >
+                  <NavLink
+                    to={prop.path}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    <i className={prop.icon} />
+                    <p>{prop.name}</p>
+                  </NavLink>
+                </li>
+              );
+            })}
+          </Nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
