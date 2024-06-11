9d5dbe436d77ac85b0b061e90db1cc63497a46d0
ridhimagu <ridhima1623@gmail.com>
2024-06-11 23:23:53 +0530

    more bug fixes

---
+++ frontend/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss?v=1.2.0";
+import "assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+
+import AdminLayout from "layouts/Admin.js";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+
+      <Redirect exact from="/frontend" to="/upload-dataset" />
+      <Redirect exact from="/" to="/upload-dataset" />
+      <Route path="/" render={(props) => <AdminLayout {...props} />} />
+
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
